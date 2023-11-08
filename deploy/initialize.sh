8be06e3e290f77eca118a8e2fcf7ad46d800d487
pisethraringsey.suon <pisethraingsey@dr-tech.com>
2023-11-08 10:04:31 +0700

    Apply ENV-VAR

--- deploy/initialize.sh
+++ deploy/initialize.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
-echo "########################################################"
-echo "##                                                    ##"
-echo "##              CREATING DOCKER IMAGE                 ##"
-echo "##                                                    ##"
-echo "########################################################"
+echo "📡📡📡 CREATING DOCKER IMAGE 📡📡📡📡"
+name='pisethraingsey'
+version='1.0-SNAPSHOT'
 packages=('configuration' 'discovery' 'endpoints')
 echo "... Cleaning and packaging "
 mvn -s settings.xml clean package
 for pgk in "${packages[@]}"
-  do echo $pgk "is ready."
-      echo ">>>>>> Create docker image of $pgk >>>>>"
-      docker build --tag=$pgk:1.0-SNAPSHOT -f deploy/$pgk/Dockerfile .
-      echo ">>>>>> Create docker images $pgk is completed."
+do echo $pgk "is ready."
+  echo "Preparing docker image building.........."
+  echo " 🛠>🛠>🛠 >>>> Create docker image of $pgk >>>>>"
+    docker build --tag=$name/$pgk:$version -f deploy/$pgk/Dockerfile .
+  echo "<<<<<< Create docker images $pgk is completed."
+  echo "Preparing pushing to docker hub.........."
+  echo "🧰 > 🧰 > 🧰 >>>> Creat tag name $pgk"
+    docker tag $version $name/$pgk:$version
+  echo "🗃 > 🗃 > 🗃 >>>> Pushing docker images $pgk to docker hub 🚢"
+    docker push $name/$pgk:$version
 done
+echo "🎉🙌🎉🙌🎉🙌🎉🙌🎉🙌🎉🙌🎉🙌🎉🙌🎉🙌🎉🙌🎉🙌🎉🙌"
+echo "🙌🎉🎉 CREATING DOCKER IMAGE COMPLETED       🎉🎉🙌  "
+echo "🎉🙌🎉🙌🎉🙌🎉🙌🎉🙌🎉🙌🎉🙌🎉🙌🎉🙌🎉🙌🎉🙌🎉🙌"
